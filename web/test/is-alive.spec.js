b06efdef216dc837323330924b88a5a57065ebcd
Mikael Arneborn <mikael@arneborn.net>
2017-04-19 18:15:30 -0700

    mocha works in web directory

---
+++ web/test/is-alive.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('foo', function() {
+  it('should pass', function() {
+    expect(true).to.equal(true);
+  });
+  it('should fail', function() {
+    expect(true).to.equal(false);
+  });
+});
