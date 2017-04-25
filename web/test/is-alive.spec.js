1012530f5607f57030f2eadd4a36657e825c44ae
Mikael Arneborn <mikael@arneborn.net>
2017-04-25 14:45:11 -0700

    mocha with babel works

--- web/test/is-alive.spec.js
+++ web/test/is-alive.spec.js
@@ -1,10 +1,11 @@
 'use strict';
 
-describe('foo', function() {
+describe('checking that mocha runs', function() {
   it('should pass', function() {
     expect(true).to.equal(true);
   });
   it('should fail', function() {
     expect(true).to.equal(false);
   });
+
 });
