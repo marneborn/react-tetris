542f1a6b0ae7b9f14c9f2d9d227d84cec7baeba2
Mikael Arneborn <mikael@arneborn.net>
2017-04-25 17:02:37 -0700

    rotate left and right

--- web/src/Shape.js
+++ web/src/Shape.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const CELLS = Symbol('cells');
+const HEIGHT = Symbol('height');
+const WIDTH = Symbol('width');
 
 class Shape {
 
@@ -13,7 +15,53 @@ class Shape {
     }
   }
 
+  get [HEIGHT] () {
+    return this[CELLS].length;
+  }
+
+  get [WIDTH]() {
+    return this[HEIGHT] > 0 ? this[CELLS][0].length : 0;
+  }
+
+  rotateRight() {
+    const H = this[HEIGHT];
+    const W = this[WIDTH];
+    let tmp = [];
+    let i, j;
+    for (i = 0; i < W; i++) {
+      tmp.push([]);
+    }
+
+    for (i = 0; i < H; i++) {
+      for (j = 0; j < W; j++) {
+        tmp[j][H - i - 1] = this[CELLS][i][j];
+      }
+    }
+    this[CELLS] = tmp;
+  }
+
+  rotateLeft() {
+    const H = this[HEIGHT];
+    const W = this[WIDTH];
+    let tmp = [];
+    let i, j;
+    for (i = 0; i < W; i++) {
+      tmp.push([]);
+    }
+
+    for (i = 0; i < H; i++) {
+      for (j = 0; j < W; j++) {
+        tmp[W - j - 1][i] = this[CELLS][i][j];
+      }
+    }
+    this[CELLS] = tmp;
+  }
+
   toJSON() {
+    return this.toString();
+    }
+
+  toString() {
     return this[CELLS]
           .map(row => row
                .map(b => b ? 'X' : '.')
@@ -23,7 +71,15 @@ class Shape {
   }
 
   fromString(rows) {
-    this[CELLS] = rows.map(stringToRow);
+    let width;
+    this[CELLS] = rows.map((string) => {
+      const row = stringToRow(string);
+      if (width === undefined) width = row.length;
+      if (width !== row.length) {
+        throw new Error('All rows must be the same width');
+      }
+      return row;
+    });
   }
 }
 
--- web/test/Shape.spec.js
+++ web/test/Shape.spec.js
@@ -13,28 +13,75 @@ describe('#Shape', function() {
       expect(JSON.stringify(shape)).to.equal('""');
     });
 
-    it('Should be able to create a shape', function() {
-      const shape = new Shape({
-        shape: [
-          'XX',
-          'X.'
-        ]
-      });
-      expect(JSON.stringify(shape)).to.equal(shapeHelpers.arrayToString([
+    it('Should be able to create a simple shape', function() {
+
+      const def = [
         'XX',
         'X.'
+      ];
+
+      const shape = new Shape({ shape: def });
+
+      expect(JSON.stringify(shape)).to.equal(shapeHelpers.arrayToString(def));
+    });
+
+    it('Should check that all rows are the same size', function() {
+      const def = [
+        'XX',
+        'X.XXXXX'
+      ];
+
+      expect(() => { new Shape({shape: def}); }).to.throw('All rows must be the same width');
+    });
+
+    it('Should be able to create a more complicated  shape', function() {
+
+      const def = [
+        '.XX',
+        '.X.',
+        '..X'
+      ];
+
+      const shape = new Shape({ shape: def });
+
+      expect(JSON.stringify(shape)).to.equal(shapeHelpers.arrayToString(def));
+    });
+
+  });
+
+  describe('.rotate', function() {
+
+    const def = [
+      '.XX',
+      '.X.',
+      '..X',
+      '...',
+      '...'
+    ];
+
+    let shape;
+    beforeEach('Create a new shape for each test', function() {
+      shape = new Shape({ shape: def });
+    });
+
+    it('Should be able to rotate right', function() {
+      shape.rotateRight();
+      expect(JSON.stringify(shape)).to.equal(shapeHelpers.arrayToString([
+        '.....',
+        '...XX',
+        '..X.X'
+      ]));
+    });
+
+    it('Should be able to rotate left', function() {
+      shape.rotateLeft();
+      expect(JSON.stringify(shape)).to.equal(shapeHelpers.arrayToString([
+        'X.X..',
+        'XX...',
+        '.....'
       ]));
     });
 
   });
 
-//   describe('constructor', function() {
-//     it('should create an
-//   });
-
-//   let shape;
-//   beforeEach('Create a new shape for each test', function() {
-//     shape = new Shape();
-//   });
-
-})
+});
